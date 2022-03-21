f48aa93eeeb9f28c717f577fa11ccc6ceb4e7eb7
vanvietan <vanvietan2911@gmail.com>
2022-03-21 14:04:39 -0700

    fix-red-mark

--- src/main/java/com/luv2code/java14/elearning/entity/library/Library.java
+++ src/main/java/com/luv2code/java14/elearning/entity/library/Library.java
@@ -1,12 +1,11 @@
 package com.luv2code.java14.elearning.entity.library;
 
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 
 import com.luv2code.java14.elearning.entity.course.Course;
@@ -23,18 +22,18 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @Entity
 @Table(name="library_course")
-public class LibraryCourse {
+public class Library {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="id")
-	private int id;
+	@EmbeddedId
+	private LibraryKey key;
 	
 	@ManyToOne
+	@MapsId("userId")
 	@JoinColumn(name="user_id")
 	private User user;
 	
 	@ManyToOne
+	@MapsId("courseId")
 	@JoinColumn(name="course_id")
 	private Course course;
 	
