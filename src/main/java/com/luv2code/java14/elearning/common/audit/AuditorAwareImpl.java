ab59aa84b9a761a6bdf4e5eb817bee1b160bf573
raymustang <91603977+raymustang@users.noreply.github.com>
2022-03-22 02:23:19 +0700

    2nd

---
+++ src/main/java/com/luv2code/java14/elearning/common/audit/AuditorAwareImpl.java
@@ -0,0 +1,29 @@
+package com.luv2code.java14.elearning.common.audit;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		Authentication auth = SecurityContextHolder
+				.getContext()
+				.getAuthentication();
+		if (auth == null)
+			return Optional.ofNullable("");
+		
+		if(auth.getPrincipal() instanceof String) {
+			String principal = (String) auth.getPrincipal();
+			return Optional.ofNullable(principal);
+		}
+			
+		UserDetails user = (UserDetails)auth.getPrincipal();
+		
+		return Optional.ofNullable(user.getUsername());
+	}
+}
---
+++ src/main/java/com/luv2code/java14/elearning/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.luv2code.java14.elearning.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.luv2code.java14.elearning.common.ResponseHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(value = {NotFoundException.class})
+	public Object handleNotFoundException(NotFoundException e) {
+		return ResponseHandler.getErrorResponse(e.getMessage()
+				, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = {Exception.class})
+	public Object handleUnexpectedException(Exception e) {
+		
+		return ResponseHandler.getErrorResponse(e.getMessage()
+				, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(value = {GiraRuntimeException.class})
+	public Object handleGiraRuntimeException(GiraRuntimeException e) {
+		return ResponseHandler.getErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+}
