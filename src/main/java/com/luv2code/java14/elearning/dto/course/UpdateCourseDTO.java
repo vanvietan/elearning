1d91f31a9a71ae5d80c05713c148451d9ef3b862
raymustang <91603977+raymustang@users.noreply.github.com>
2022-03-22 02:28:52 +0700

    4th

--- src/main/java/com/luv2code/java14/elearning/dto/course/UpdateCourseDTO.java
+++ src/main/java/com/luv2code/java14/elearning/dto/course/UpdateCourseDTO.java
@@ -1,7 +1,5 @@
 package com.luv2code.java14.elearning.dto.course;
 
-import javax.validation.constraints.NotBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,12 +14,10 @@ import lombok.ToString;
 @ToString
 @Builder
 public class UpdateCourseDTO {
-	@NotBlank(message = "{Course name can not be blank")
+	
 	private String name;
 	
-	@NotBlank(message = "Course info can not be blank")
 	private String courseInfo;
 
-	@NotBlank(message = "Course price can not be blank")
 	private int price;
 }
