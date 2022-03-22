f3d0f94fd9655685ed03a1f459c96d55c8941e35
vanvietan <vanvietan2911@gmail.com>
2022-03-21 19:15:36 -0700

    rate-course

--- src/main/java/com/luv2code/java14/elearning/controller/library/LibraryController.java
+++ src/main/java/com/luv2code/java14/elearning/controller/library/LibraryController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 public interface LibraryController {
 	
@@ -23,5 +24,12 @@ public interface LibraryController {
 	public ResponseEntity<Object> deleteCourseFromLibrary(
 			@PathVariable("courseId") int courseId,
 			@PathVariable("userId") int userId
+			);
+	
+	@PutMapping(value="/library/{userId}/{courseId}/{rating}")
+	public ResponseEntity<Object> rateTheCourse(
+			@PathVariable("courseId") int courseId,
+			@PathVariable("userId") int userId,
+			@PathVariable("rating") int rating
 			);
 }
